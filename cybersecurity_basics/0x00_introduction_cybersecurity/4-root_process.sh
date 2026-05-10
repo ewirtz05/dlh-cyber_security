47afb5a0488a28a2feb8e9bd2df1a89ceffa3b48
ewirtz05 <wirer191@gmail.com>
2026-05-10 11:07:52 -0400

    modified script to contain 2 lines only

--- cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <username>"
-    exit 1
-fi
-USER_NAME=$1
-ps -u "$USER_NAME" -o user,%cpu,%mem,vsz,rss,tty,stat,start,time,command | grep -v " 0 0 "
+ps -u "$1" -o user,%cpu,%mem,vsz,rss,tty,stat,start,time,command | grep -v " 0 0 "
