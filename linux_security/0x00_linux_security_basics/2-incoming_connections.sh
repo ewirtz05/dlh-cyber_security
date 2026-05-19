17e30fce9e8d788b25546311636dc51a70af435b
ewirtz05 <wirer191@gmail.com>
2026-05-19 10:43:38 -0400

    reduce script to 2 lines

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-sudo iptables -F
-sudo iptables -P INPUT DROP
-sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-echo "Rules updated"
+sudo iptables -F && sudo iptables -P INPUT DROP && sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
