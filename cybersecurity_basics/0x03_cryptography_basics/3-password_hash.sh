33954bfd80813ffd44a6e96f276ef1beef64e1aa
ewirtz05 <wirer191@gmail.com>
2026-06-07 08:51:36 -0400

    fixed secure password hash script

--- cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
+++ cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-password="$1"
-salt=$(openssl rand -hex 8)
-echo -n "${password}${salt}" | openssl dgst -sha512 > 3_hash.txt
+echo -n "$1$(openssl rand -hex 8)" | openssl dgst -sha512 > 3_hash.txt
