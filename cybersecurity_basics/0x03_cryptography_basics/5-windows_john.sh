72806148838502a00b5983f43164b0d141136d39
ewirtz05 <wirer191@gmail.com>
2026-06-07 09:18:06 -0400

    fixed Windows NT hash cracking script

--- cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt --format=NT "$1" && john --show --format=NT "$1" | grep -v "password hash" | cut -d: -f2 > 5-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt --format=NT "$1" >/dev/null 2>&1; john --show --format=NT "$1" | awk -F: '/:/ {print $2; exit}' > 5-password.txt
