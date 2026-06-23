1ee609a8f59fe231b17ff09d2d7c360f571628a8
ewirtz05 <wirer191@gmail.com>
2026-06-23 11:55:37 -0400

    Fixed subfinder

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-subfinder -d "$1" -silent | while read subdomain; do ip=$(host "$subdomain" | grep "has address" | head -n 1 | awk '{print $4}'); [ -n "$ip" ] && echo "$subdomain, $ip"; done > domain.txt
+subfinder -silent -d $1 -o $1.txt -nW -oI
