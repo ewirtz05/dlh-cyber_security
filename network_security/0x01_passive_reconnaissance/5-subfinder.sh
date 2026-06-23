9f08e29047d09992153e12326afbfd82985fd8ae
ewirtz05 <wirer191@gmail.com>
2026-06-23 10:32:26 -0400

    Fixed subfinder passive reconnaissance script

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-subfinder -d "$1" -silent | while read sub; do ip=$(dig +short "$sub" A | head -n 1); [ -n "$ip" ] && echo "$sub,$ip"; done > "${1%%.*}.txt"
+subfinder -d "$1" -silent | while read sub; do ip=$(dig +short "$sub" A | head -n 1); [ -n "$ip" ] && echo "$sub,$ip"; done > domain.txt
