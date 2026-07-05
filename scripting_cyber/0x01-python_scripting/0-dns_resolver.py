eaa7a08991b56c824b5bd25296a78e91415f656e
ewirtz05 <wirer191@gmail.com>
2026-07-05 03:20:37 -0400

    add IPv4 DNS resolver

---
+++ scripting_cyber/0x01-python_scripting/0-dns_resolver.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import socket
+
+def resolve_domain_to_ip4(domain_name):
+	"""
+	Resolve a domain name to its IP4 address
+	
+	Args:
+		domain_name (str)
+	
+	Returns:
+		str: IP4 if resolved successfully
+		None: If the domain cannot be resolved
+		str: Error message for exceptions
+	"""
+	try:
+		return socket.gethostbyname(domain_name)
+	except socket.gaierror:
+		return None
+	except Exception as error:
+		return str(error)
+
+if __name__ == "__main__":
+	pass
